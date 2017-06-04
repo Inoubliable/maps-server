4e9672756419a1a99c4c8c034d29be3c652ef54b
Tim <tim.janzelj@gmail.com>
2017-06-05 00:08:04 +0200

    Removing socket.io and adding REST urls

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 Server url: https://peaceful-taiga-88033.herokuapp.com
 
-/users - get all users
+GET /users - get all users
+GET /login - add current user to db and get his id
+POST /users/:id - update user with id
--- index.js
+++ index.js
@@ -3,7 +3,6 @@ var app = express();
 var path = require('path');
 var http = require('http').Server(app);
 var bodyParser = require('body-parser')
-var io = require('socket.io')(http);
 
 app.use(bodyParser.json()); // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({extended: true})); // to support URL-encoded bodies
@@ -12,6 +11,19 @@ app.use(bodyParser.urlencoded({extended: true})); // to support URL-encoded bodi
 var db = require('./config/database');
 var firebase = db.database();
 
+app.get('/login', (req, res) => {
+	// insert new user into db
+	var newUserRef = firebase.ref().child('users').push();
+	newUserRef.set({
+		lat: 0,
+		lng: 0
+	});
+
+	// send him his id
+	res.json({id: newUserRef.key});
+
+	res.end();
+});
 app.get('/users', (req, res) => {
 	var usersRef = firebase.ref().child('users');
 
@@ -29,7 +41,7 @@ app.get('/users', (req, res) => {
 		res.json(parsedUsers);
 	});
 });
-app.get('/users/:id', (req, res) => {
+app.post('/users/:id', (req, res) => {
 	var userId = req.params.id;
 	var lat = req.body.lat;
 	var lng = req.body.lng;
@@ -53,40 +65,6 @@ app.get('/add', (req, res) => {
 
 	res.end();
 });
-
-io.on('connection', onConnection);
-
-function onConnection(socket) {
-	// insert new user into db
-	var newUserRef = firebase.ref().child('users').push();
-	newUserRef.set({
-		lat: 0,
-		lng: 0
-	});
-
-	// send him his id
-	console.log(newUserRef.key);
-	io.to(socket.id).emit('my id', newUserRef.key);
-};
-
-setInterval(function() {
-	var usersRef = firebase.ref().child('users');
-
-	usersRef.once('value').then((snapshot) => {
-		var users = snapshot.val() || {};
-		var keys = Object.keys(users);
-		var parsedUsers = [];
-		var lastUser;
-		keys.forEach((key) => {
-			parsedUsers.push(users[key]);
-			lastUser = parsedUsers.slice(-1)[0];
-			lastUser.id = key;
-		});
-
-		io.emit('update', {users: users});
-	});
-	
-}, 5000);
 
 const PORT = process.env.PORT || 3000;
 http.listen(PORT, () => {
