5c6600567afd5766afbd202ea1a50b6d6518e125
Tim <tim.janzelj@gmail.com>
2017-06-06 23:07:04 +0200

    Adding support for LPTparkirisca

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ var express = require('express');
 var app = express();
 var path = require('path');
 var http = require('http').Server(app);
-var bodyParser = require('body-parser')
+var bodyParser = require('body-parser');
+var request = require('request');
+var cheerio = require('cheerio');
 
 app.use(bodyParser.json()); // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({extended: true})); // to support URL-encoded bodies
@@ -24,6 +26,7 @@ app.get('/login', (req, res) => {
 });
 
 app.get('/users', (req, res) => {
+	var testStart = Date.now();
 	// if all query params are specified, return users within range
 	if(req.query.lat && req.query.lng && req.query.r) {
 		var centerLat = parseFloat(req.query.lat);
@@ -51,7 +54,8 @@ app.get('/users', (req, res) => {
 					lastUser.id = key;
 				}
 			});
-
+			var testEnd = Date.now();
+			console.log(testEnd - testStart);
 			res.json(parsedUsers);
 		});
 	} else { // if not all query params are specified, return all users
@@ -87,24 +91,93 @@ app.post('/users/:id', (req, res) => {
 	var userId = req.params.id;
 	var lat = req.body.lat;
 	var lng = req.body.lng;
-	var usersRef = firebase.ref('users/' + userId).update({ lat: lat, lng: lng });
+	firebase.ref('users/' + userId).update({ lat: lat, lng: lng });
 
 	res.end();
 });
 
-// adding users for testing
-var testLat = 23;
-var testLng = 12;
-app.get('/add', (req, res) => {
-	var newUserRef = firebase.ref().child('users').push();
-	newUserRef.set({
-		lat: testLat,
-		lng: testLng,
-		time: Date.now()
+var allParkings = {
+	"Petkovškovo nabrežje II.": {lat: 46.0521351, lng: 14.5093453, all: 0, available: 0},
+	"NUK II.": {lat: 46.0463566, lng: 14.5017598, all: 0, available: 0},
+	"Senatorij Emona": {lat: 46.0535596, lng: 14.5064339, all: 0, available: 0},
+	"Bežigrad": {lat: 46.0625096, lng: 14.5060959, all: 0, available: 0},
+	"Mirje": {lat: 46.0466615, lng: 14.4921755, all: 0, available: 0},
+	"Trg MDB": {lat: 46.0469197, lng: 14.4935305, all: 0, available: 0},
+	"Tivoli I.": {lat: 46.0576398, lng: 14.4968741, all: 0, available: 0},
+	"Tivoli II.": {lat: 46.060125, lng: 14.4950341, all: 0, available: 0},
+	"Žale I.": {lat: 46.0689395, lng: 14.5267334, all: 0, available: 0},
+	"Žale II.": {lat: 46.0676294, lng: 14.527463, all: 0, available: 0},
+	"Žale III. - Soča": {lat: 46.0682057, lng: 14.5236963, all: 0, available: 0},
+	"Žale IV.": {lat: 46.0695067, lng: 14.5248611, all: 0, available: 0},
+	"Trg prekomorskih brigad": {lat: 46.0691515, lng: 14.4872436, all: 0, available: 0},
+	"Kranjčeva (Pop TV)": {lat: 46.0702222, lng: 14.5199124, all: 0, available: 0},
+	"Štembalova ulica": {lat: 46.0808662, lng: 14.5105652, all: 0, available: 0},
+	"Gosarjeva ulica": {lat: 46.0746331, lng: 14.513012, all: 0, available: 0},
+	"Gosarjeva ulica II.": {lat: 46.0740417, lng: 14.5153521, all: 0, available: 0},
+	"Kozolec": {lat: 46.0567934, lng: 14.5027459, all: 0, available: 0},
+	"Kongresni trg": {lat: 46.0506341, lng: 14.501298, all: 0, available: 0},
+	"PH Kolezija": {lat: 46.0423455, lng: 14.4926678, all: 0, available: 0},
+	"Center Stožice": {lat: 46.0793676, lng: 14.5221019, all: 0, available: 0},
+	"Gospodarsko razstavišče": {lat: 46.0618602, lng: 14.5079083, all: 0, available: 0},
+	"Gospodarsko razstavišče - Abonenti": {lat: 46.0618602, lng: 14.5079083, all: 0, available: 0},
+	"Linhartova": {lat: 46.0632226, lng: 14.5077204, all: 0, available: 0},
+	"P+R Studenec": {lat: 46.0541576, lng: 14.5652754, all: 0, available: 0},
+	"P+R Dolgi Most": {lat: 46.0365865, lng: 14.4611001, all: 0, available: 0},
+	"P+R Barje": {lat: 46.0270564, lng: 14.4982871, all: 0, available: 0},
+	"Območja čas. om. park.": {lat: 0, lng: 0, all: 0, available: 0}
+};
+
+function scrapeParkings(callback) {
+	var parkiriscaUrl = 'http://www.lpt.si/parkirisca/parkirisca';
+
+	request(parkiriscaUrl, function(error, response, html){
+
+        if(!error){
+
+            var $ = cheerio.load(html);
+            var parkirisca = $('tr.table_list');
+            var name, all, available;
+
+            parkirisca.each(function() {
+            	name = $(this).find('.occupancy_name_td').text();
+            	all = parseInt($(this).find('.occupancy_daily:nth-child(3) .occupancy_td').text());
+            	available = parseInt($(this).find('.occupancy_daily:nth-child(4) .occupancy_td').text());
+
+            	if(!available) {
+            		available = 0;
+            	}
+
+            	allParkings[name].all = all;
+            	allParkings[name].available = available;
+            });
+        }
+
+        callback();
+    });
+};
+
+app.get('/parkings', (req, res) => {
+
+	scrapeParkings(function() {
+		res.json(allParkings)
 	});
 
-	testLat++;
-	testLng++;
+});
+
+// add testing users
+app.get('/addTestUsers/:num', (req, res) => {
+	var num = req.params.num;
+	var testLat, testLng;
+	for (i = 0; i < num; i++) {
+		testLat = Math.random() * 170 - 85;
+		testLng = Math.random() * 360 - 180;
+		var newUserRef = firebase.ref().child('users').push();
+		newUserRef.set({
+			lat: testLat,
+			lng: testLng,
+			time: Date.now()
+		});
+	}
 
 	res.end();
 });
